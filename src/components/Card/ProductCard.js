8299c68458b8da3694cfc76bb8fdf753693126ab
popovicmarko <popovicmarko350@gmail.com>
2023-05-30 13:16:20 +0200

    extended json file and cart logic done

--- src/components/Card/ProductCard.js
+++ src/components/Card/ProductCard.js
@@ -1,48 +1,69 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import "./ProductCard.css";
+import { CardActions, CardMedia } from "@mui/material";
+import DeleteButton from "../Button/DeleteButton";
+import { AppContext } from "../../context/AppContext";
 
 export default function ProductCard({
+  id,
   productName,
   productPrice,
   productImage,
-  onPress,
+  addToCart,
+  deleteFromCart,
 }) {
+  const { cart } = useContext(AppContext);
+  const [isAdded, setIsAdded] = useState(false);
+
+  useEffect(() => {
+    if (!!cart.find((product) => product.id === id)) {
+      setIsAdded(true);
+    } else {
+      setIsAdded(false);
+    }
+  }, [id, cart]);
+  // console.log({ isAdded });
+
   return (
-    <Card sx={{ width: 400, height: 450 }}>
-      <img
-        style={{ height: 300, width: 400, objectFit: "cover" }}
-        src={productImage}
-        alt="product"
-      />
+    <Card sx={{ maxWidth: 450 }}>
+      <CardMedia sx={{ height: 250 }} image={productImage} title="" />
       <CardContent>
-        <div style={{ height: 70 }}>
-          <Typography gutterBottom variant="h5" component="div">
-            {productName}
-          </Typography>
-        </div>
-        <div className="description">
-          <Typography
-            variant="body1"
-            color="text.primary"
-            size="large"
-            fontSize={20}
-            fontWeight={600}
-          >
-            Price: {productPrice} EUR
-          </Typography>
-          <Button
-            size="medium"
-            variant="outlined"
-            color="success"
-            onClick={onPress}
-          >
-            Add to Cart
-          </Button>
-        </div>
+        <Typography
+          gutterBottom
+          variant="h5"
+          component="div"
+          style={{ height: "55px" }}
+        >
+          {productName}
+        </Typography>
+        <Typography
+          gutterBottom
+          variant="h5"
+          component="div"
+          className="product-price"
+          style={{ fontWeight: "700" }}
+        >
+          {productPrice}
+          EUR
+        </Typography>
+        <CardActions style={{ display: "flex", justifyContent: "center" }}>
+          {!isAdded ? (
+            <Button
+              style={{ color: "#2e5b36", fontWeight: "700" }}
+              onClick={addToCart}
+              size="small"
+              className="btn-add"
+            >
+              ADD TO CART
+            </Button>
+          ) : (
+            <DeleteButton onDelete={deleteFromCart} />
+          )}
+        </CardActions>
       </CardContent>
     </Card>
   );
--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,5 +1,66 @@
 import React from "react";
+import { useContext } from "react";
+import IconButton from "@mui/material/IconButton";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import { AppContext } from "../../context/AppContext";
+import CartCards from "../../components/CartCards/CartCards";
 
 export default function Cart() {
-  return <div>Cart</div>;
+  const { cart } = useContext(AppContext);
+
+  return (
+    <>
+      {cart.length !== 0 ? (
+        <div className="cartCards">
+          {/* <p>producstii</p> */}
+          <h1 style={{ textAlign: "center", borderBottom: "1px solid green" }}>
+            Your cart
+          </h1>
+          {cart.map((product) => (
+            <CartCards
+              key={product.id}
+              id={product.id}
+              productImage={product.imageURL}
+              productName={product.title}
+              productPrice={product.price}
+              quantity={product.quantityInCart}
+            />
+          ))}
+        </div>
+      ) : (
+        <div>
+          <div
+            style={{
+              height: "70vh",
+              backgroundImage: `url("https://www.adasglobal.com/img/empty-cart.png")`,
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "50vw",
+            }}
+          ></div>
+          <IconButton
+            style={{
+              marginTop: "-40px",
+              backgroundColor: "green",
+              borderRadius: "5px",
+            }}
+            color=""
+            aria-label="add to shopping cart"
+          >
+            <a
+              style={{
+                color: "#ffffff",
+                textDecoration: "none",
+                fontWeight: "700",
+              }}
+              href="products"
+            >
+              Go shopping
+            </a>
+            <AddShoppingCartIcon />
+          </IconButton>
+        </div>
+      )}
+    </>
+  );
 }
